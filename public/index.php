460ebe1940a828e69a9e917046e43e07d267bd2e
viniciussdudu <166521381+viniciussdudu@users.noreply.github.com>
2026-03-25 02:00:12 -0300

    Revert "Feature/layouts eedição"

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../src/Model/Cardapio.php';
+require_once __DIR__ . '/../src/Model/cardapio.php';
 require_once __DIR__ . '/../src/Controller/cardapioController.php';
 
 use App\Controller\cardapioController;
 
 $controller = new CardapioController();
 
-$pagina = $_GET['pagina'] ?? $_POST['pagina'] ?? 'inicio';
+$pagina = $GET['pagina'] ?? 'inicio';
 
 if ($pagina === 'almoço') {
     $controller->exibiralmoço();
@@ -15,15 +15,6 @@ if ($pagina === 'almoço') {
 elseif ($pagina === 'janta') {
     $controller->exibirJanta();
 }
-elseif ($pagina === 'atualizarcardapio') {
-    $controller->exibirAtualizarCardapio();
-}
-elseif ($pagina === 'salvar') {
-    $controller->salvar();
-}
 else {
-    $dataHoje = date('d/m/Y');
-    $dados = new \App\Model\Cardapio();
-    $cardapio = $dados->obterRefeicoes();
     $controller->exibirHome();
 }
