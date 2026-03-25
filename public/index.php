8a8ac9799fb5d92ab0ada85742e08f9416baeea8
Klaus1215 <klaussouza1215@gmail.com>
2026-03-24 23:28:06 -0300

    adicionando a função do almoço

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../src/Model/cardapio.php';
+require_once __DIR__ . '/../src/Model/Cardapio.php';
 require_once __DIR__ . '/../src/Controller/cardapioController.php';
 
 use App\Controller\cardapioController;
---
+++ src/Controller/cardapioController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use App\Model\Cardapio;
+
+class CardapioController {
+    private $model;
+
+    public function __construct() {
+        $this->model = new Cardapio();
+    }
+
+     public function exibirHome() {
+         include __DIR__ . '/../View/home.php';
+     }
+
+    public function exibiralmoço() {
+        $dados = $this->model->obterRefeicoes();
+        $refeicao = $dados['almoco'];
+        include __DIR__ . '/../View/cardapiodetalhes.php';
+    }
+
+}
